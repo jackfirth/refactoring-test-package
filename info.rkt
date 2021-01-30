d98dfd3a81ef2b8b396102671e262a4de7e6a285
Jack Firth <jackhfirth@gmail.com>
2021-01-29 20:26:39 -0800

    Fill out template

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
-(define collection "racket-package-template")
+(define collection "refactoring-test-package")
 
 (define scribblings
   (list (list "main.scrbl"
               (list 'multi-page)
               (list 'library)
-              "racket-package-template")))
+              "refactoring-test-package")))
 
 (define deps
   (list "base"))
--- main.scrbl
+++ main.scrbl
@@ -1,4 +1,4 @@
 #lang scribble/manual
 
-@title{Racket Package Template}
-@defmodule[racket-package-template]
+@title{Refactoring Test Package}
+@defmodule[refactoring-test-package]
